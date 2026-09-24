d112e719fb1ecc84a62a84b00fc699ee05178137
Redxman47 <jacobovalencia47@gmail.com>
2026-09-23 19:17:49 -0500

    Refactor Compra class and add toString method

--- src/main/java/Compra.java
+++ src/main/java/Compra.java
@@ -1,68 +1,78 @@
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-public class Compra {
-    private String codigo;
-    private LocalDate fecha;
-    private MetodoPago metodoPago;
-    private double valorTotal;
-    private List<Producto> listaCompraProductos;
-
-
-    public Compra(String codigo, LocalDate fecha, MetodoPago metodoPago) {
-        this.codigo = codigo;
-        this.fecha = fecha;
-        this.metodoPago = metodoPago;
-        this.valorTotal = valorTotal;
-        this.listaCompraProductos = new ArrayList<>();
-    }
-
-
-    public String getCodigo() {
-        return codigo;
-    }
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-    public LocalDate getFecha() {
-        return fecha;
-    }
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-    public MetodoPago getMetodoPago() {
-        return metodoPago;
-    }
-    public void setMetodoPago(MetodoPago metodoPago) {
-        this.metodoPago = metodoPago;
-    }
-    public double getValorTotal() {
-        return valorTotal;
-    }
-    public List<Producto> getListaCompraProductos() {
-        return listaCompraProductos;
-    }
-    public void setListaCompraProductos(List<Producto> listaComprarProductos) {
-        this.listaCompraProductos = listaComprarProductos;
-    }
-    public boolean agregarProducto(Producto producto) {
-        listaCompraProductos.add(producto);
-        return false;
-    }
-    public double calcularValorTotal() {
-        double valorTotal = 0;
-        {
-            for (Producto producto : listaCompraProductos) {
-                valorTotal += producto.getPrecioUnitario();
+    import java.time.LocalDate;
+    import java.util.ArrayList;
+    import java.util.List;
+    public class Compra {
+        private String codigo;
+        private LocalDate fecha;
+        private MetodoPago metodoPago;
+        private double valorTotal;
+        private List<Producto> listaCompraProductos;
+    
+    
+        public Compra(String codigo, LocalDate fecha, MetodoPago metodoPago) {
+            this.codigo = codigo;
+            this.fecha = fecha;
+            this.metodoPago = metodoPago;
+            this.valorTotal = valorTotal;
+            this.listaCompraProductos = new ArrayList<>();
+        }
+    
+    
+        public String getCodigo() {
+            return codigo;
+        }
+        public void setCodigo(String codigo) {
+            this.codigo = codigo;
+        }
+        public LocalDate getFecha() {
+            return fecha;
+        }
+        public void setFecha(LocalDate fecha) {
+            this.fecha = fecha;
+        }
+        public MetodoPago getMetodoPago() {
+            return metodoPago;
+        }
+        public void setMetodoPago(MetodoPago metodoPago) {
+            this.metodoPago = metodoPago;
+        }
+        public double getValorTotal() {
+            return valorTotal;
+        }
+        public List<Producto> getListaCompraProductos() {
+            return listaCompraProductos;
+        }
+        public void setListaCompraProductos(List<Producto> listaComprarProductos) {
+            this.listaCompraProductos = listaComprarProductos;
+        }
+        public boolean agregarProducto(Producto producto) {
+            listaCompraProductos.add(producto);
+            return false;
+        }
+        public double calcularValorTotal() {
+            double valorTotal = 0;
+            {
+                for (Producto producto : listaCompraProductos) {
+                    valorTotal += producto.getPrecioUnitario();
+                }
             }
+            return valorTotal;
         }
-        return valorTotal;
-    }
-    public void confirmarCompra() {
-        for (Producto producto : listaCompraProductos) {
-            producto.validarDisponibilidad(1);
+        public void confirmarCompra() {
+            for (Producto producto : listaCompraProductos) {
+                producto.validarDisponibilidad(1);
+            }
+            this.valorTotal = calcularValorTotal();
+            System.out.println("Compra confirmada. Valor total: " + this.valorTotal + "");
         }
-        this.valorTotal = calcularValorTotal();
-        System.out.println("Compra confirmada. Valor total: " + this.valorTotal + "");
+        @Override
+        public String toString() {
+            return "Compra{" +
+                    "codigo='" + codigo + '\'' +
+                    ", fecha=" + fecha +
+                    ", metodoPago=" + metodoPago +
+                    ", valorTotal=" + valorTotal +
+                    ", lista de Compra de Productos=" + listaCompraProductos +
+                    '}';
+        }
     }
-}
