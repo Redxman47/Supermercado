0a80effbcbf2f0eea925539885af9c47a7525a0f
Redxman47 <jacobovalencia47@gmail.com>
2026-09-23 19:11:26 -0500

    Add documento field to Cliente class

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,61 +1,71 @@
-import java.util.ArrayList;
-import java.util.List;
-public class Cliente {
-    private String nombreCompleto;
-    private String direccion;
-    private String telefono;
-    private String correo;
-    private List<Compra> listaCompras;
+    import java.util.ArrayList;
+    import java.util.List;
+    public class Cliente {
+        private String nombreCompleto;
+        private String documento;
+        private String direccion;
+        private String telefono;
+        private String correo;
+        private List<Compra> listaCompras;
+    
+        public Cliente(String nombreCompleto,String documento, String direccion, String telefono, String correo) {
+            this.nombreCompleto = nombreCompleto;
+            this.documento = documento;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.correo = correo;
+            this.listaCompras = new ArrayList<>();
+        }
+        public String getNombreCompleto() {
+            return nombreCompleto;
+        }
+        public void setNombreCompleto(String nombreCompleto) {
+            this.nombreCompleto = nombreCompleto;
+        }
+        public String getDocumento() {
+            return documento;
+        }
+        public void setDocumento(String documento) {
+            this.documento = documento;
+        }
+        public String getDireccion() {
+            return direccion;
+        }
+        public void setDireccion(String direccion) {
+            this.direccion = direccion;
+        }
+        public String getTelefono() {
+            return telefono;
+        }
+        public void setTelefono(String telefono) {
+            this.telefono = telefono;
+        }
+        public String getCorreo() {
+            return correo;
+        }
+        public void setCorreo(String correo) {
+            this.correo = correo;
+        }
+        public List<Compra> getListaCompras() {
+            return listaCompras;
+        }
+        public void setListaCompras(List<Compra> listaCompras) {
+            this.listaCompras = listaCompras;
+        }
+        public void agregarCompra(Compra compra) {
+            listaCompras.add(compra);
+        }
+    
+        @Override
+        public String toString() {
+            return "Cliente{" +
+                    "nombreCompleto='" + nombreCompleto + '\'' +
+                    ", documento='" + documento + '\'' +
+                    ", direccion='" + direccion + '\'' +
+                    ", telefono='" + telefono + '\'' +
+                    ", correo='" + correo + '\'' +
+                    ", listaCompras=" + listaCompras +
+                    '}';
+        }
+    }
 
-    public Cliente(String nombreCompleto, String direccion, String telefono, String correo) {
-        this.nombreCompleto = nombreCompleto;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        this.correo = correo;
-        this.listaCompras = new ArrayList<>();
-    }
-    public String getNombreCompleto() {
-        return nombreCompleto;
-    }
-    public void setNombreCompleto(String nombreCompleto) {
-        this.nombreCompleto = nombreCompleto;
-    }
-    public String getDireccion() {
-        return direccion;
-    }
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-    public String getTelefono() {
-        return telefono;
-    }
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-    public String getCorreo() {
-        return correo;
-    }
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-    public List<Compra> getListaCompras() {
-        return listaCompras;
-    }
-    public void setListaCompras(List<Compra> listaCompras) {
-        this.listaCompras = listaCompras;
-    }
-    public void agregarCompra(Compra compra) {
-        listaCompras.add(compra);
-    }
-
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "nombreCompleto='" + nombreCompleto + '\'' +
-                ", direccion='" + direccion + '\'' +
-                ", telefono='" + telefono + '\'' +
-                ", correo='" + correo + '\'' +
-                ", listaCompras=" + listaCompras +
-                '}';
-    }
-}
