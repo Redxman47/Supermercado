1b45f8638839af6708f8d90006c4ddf03e162b1f
Redxman47 <jacobovalencia47@gmail.com>
2026-09-23 19:03:36 -0500

    Add Cliente class with properties and methods

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,2 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Cliente {
+    private String nombreCompleto;
+    private String direccion;
+    private String telefono;
+    private String correo;
+    private List<Compra> listaCompras;
+
+    public Cliente(String nombreCompleto, String direccion, String telefono, String correo) {
+        this.nombreCompleto = nombreCompleto;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.listaCompras = new ArrayList<>();
+    }
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public List<Compra> getListaCompras() {
+        return listaCompras;
+    }
+    public void setListaCompras(List<Compra> listaCompras) {
+        this.listaCompras = listaCompras;
+    }
+    public void agregarCompra(Compra compra) {
+        listaCompras.add(compra);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nombreCompleto='" + nombreCompleto + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", correo='" + correo + '\'' +
+                ", listaCompras=" + listaCompras +
+                '}';
+    }
 }
